99ab43bedc62e91c75ff9b3fa81ae33c47f6955b
Igor Triveloni <96480753+IgorTriveloni@users.noreply.github.com>
2022-05-26 15:37:00 -0300

    alterações

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold" :style="estilos">
+        <!-- Oque tiver dentro de <Box> sera renderizado de Tarefa.vue -->
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'Box-Modelo',
+    data() {
+        return {
+            estilos: {
+                backgroundColor: '#faf0ca'
+            }
+        }
+    }
+});
+</script>
---
+++ src/components/Cronometro.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <strong class="display">{{ tempoDecorrido }}</strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Cronometro-Modelo",
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            default: 0
+        }
+    },  
+    computed: {
+        //00:00:00 cronometro
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+        },
+    },
+});
+</script>
+
+<style scope>
+.display {
+    color: var(--texto-primario);
+}
+</style>
